e3937d94d137e4872888b4493e6b5244506eb1cf
Manthan Shah <shahmanthan1303@gmail.com>
2023-04-11 10:16:35 +0530

    Add files via upload

---
+++ Day_1/main.py
@@ -0,0 +1,40 @@
+# Day 1 Practice : Printing, Input, Variable 
+
+"""Day 1.2: Printing Practice
+print("Day 1 - String Manipulation") 
+print('String Concatination is done with the "+" sing.')
+print('e.g print("Hello" + "World")')
+print("New lines can be added with a backslash and n.")"""
+
+# First it went inside the print statement and then saw "Hello" after that it saw an input statement so it first took the input from the user and then conversted the input statement into the value that the user has entered.
+#print("Hello " + input("What is your name? ") + " !")
+
+
+#Day1.3:Input Practice
+"""print(len(input("What's your Name? ")))"""
+
+#Day 1.4: Variable 
+"""name = input("what is you name? ")
+length = len(name)
+print(length)"""
+
+# Day 1.4: Practice
+"""a = input("a: ")
+b = input("b: ")
+c = a
+a = b
+b = c
+print("a = " + a)
+print("b = " + b)"""
+
+#Task 1: Band_Name_Generator
+print("Welcome to the Band Name Generator.")
+cityName = input("What is the name of the city you grew up in? ")
+print(cityName)
+petName = input("What is the name of your pet? ")
+print(petName)
+bandName  = (cityName + " " + petName)
+print("Name of your band could be : " + bandName)
+
+
+
